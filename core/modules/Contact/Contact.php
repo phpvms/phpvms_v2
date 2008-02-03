8dfa70bf1748463b2cd37eb865e3acfbb132c725
nshahzad <nabeel@nsslive.net>
2008-02-03 00:23:46 +0000

    Run HTML from template
    git-svn-id: file:///var/svn/phpvms/trunk@16 e188d3d8-5a48-0410-b2de-ed2772025908

--- core/modules/Contact/Contact.php
+++ core/modules/Contact/Contact.php
@@ -23,40 +23,7 @@ class Contact extends ModuleBase
 		// Main function
 		if($_GET['page'] == "contact")
 		{
-			echo "<form name='form1' method='post' action='index.php'>
-  <table width='100%' border='0'>
-    <tr>
-      <td>Name:</td>
-      <td><label>
-        <input type='text' name='name' id='name'>
-      </label></td>
-    </tr>
-    <tr>
-      <td>E-Mail Address:</td>
-      <td><label>
-        <input type='text' name='email' id='email'>
-      </label></td>
-    </tr>
-    <tr>
-      <td>Message:</td>
-      <td><label>
-        <textarea name='message' id='message' cols='45' rows='5'></textarea>
-      </label></td>
-    </tr>
-    <tr>
-      <td>&nbsp;</td>
-      <td>&nbsp;</td>
-    </tr>
-    <tr>
-      <td colspan='2'><label>
-        <div align='center'>
-          <input type='submit' name='button' id='button' value='Send Message'>
-        </div>
-      </label></td>
-    </tr>
-  </table><input name='contact' type='hidden' value='contact'>
-</form>
-";
+		$this->TPL->ShowTemplate("form.tpl");		
 		}
 				if(isset($_POST['contact']))
 		{

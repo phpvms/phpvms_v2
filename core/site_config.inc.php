e9fa76b95503654e4c1eb176da35cf12bae2f843
nshahzad <nabeel@nsslive.net>
2008-02-29 16:47:42 +0000

    git-svn-id: file:///var/svn/phpvms/trunk@71 e188d3d8-5a48-0410-b2de-ed2772025908

---
+++ core/site_config.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Just enter the module name
+*/
+
+define('TABLE_PREFIX', 'phpvms_');
+
+//database info
+define('DBASE_USER', '');
+define('DBASE_PASS', '');
+define('DBASE_NAME', '');
+define('DBASE_SERVER', ''); 
+
+?>

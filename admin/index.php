3d0977f83743430e5944acb5334fcee265c881c1
nshahzad <nabeel@nsslive.net>
2008-03-31 20:26:50 +0000

    git-svn-id: file:///var/svn/phpvms/trunk@257 e188d3d8-5a48-0410-b2de-ed2772025908

--- admin/index.php
+++ admin/index.php
@@ -24,11 +24,11 @@ MainController::loadModules($ADMIN_MODULES);
 $BaseTemplate = new TemplateSet;
 
 //load the main skin
-$settings_file = SITE_ROOT . '/lib/skins/crystal/crystal.php';
+$settings_file = SITE_ROOT . '/lib/skins/crystalsidebar/crystal.php';
 if(file_exists($settings_file))
 	include $settings_file;
 	
-$BaseTemplate->template_path = SITE_ROOT . '/lib/skins/crystal';
+$BaseTemplate->template_path = SITE_ROOT . '/lib/skins/crystalsidebar';
 $BaseTemplate->Set('title', SITE_NAME);
 
 Template::Set('MODULE_NAV_INC', $NAVBAR);

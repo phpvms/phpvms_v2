9e83a8a511276aae4a5cba45df0e78a84fbb63d6
nshahzad <nabeel@nsslive.net>
2009-02-16 16:40:16 +0000

    test
    git-svn-id: file:///var/svn/phpvms/trunk@615 e188d3d8-5a48-0410-b2de-ed2772025908

--- install/install.php
+++ install/install.php
@@ -1,10 +1,21 @@
 <?php
 /**
- * phpVMS Installer File
+ * phpVMS - Virtual Airline Administration Software
+ * Copyright (c) 2008 Nabeel Shahzad
+ * For more information, visit www.phpvms.net
+ *	Forums: http://www.phpvms.net/forum
+ *	Documentation: http://www.phpvms.net/docs
+ *
+ * phpVMS is licenced under the following license:
+ *   Creative Commons Attribution Non-commercial Share Alike (by-nc-sa)
+ *   View license.txt in the root, or visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+ *
+ * @author Nabeel Shahzad
+ * @copyright Copyright (c) 2008, Nabeel Shahzad
+ * @link http://www.phpvms.net
+ * @license http://creativecommons.org/licenses/by-nc-sa/3.0/
  */
-/* error_reporting(E_ALL ^ E_NOTICE);
- ini_set('display_errors', 'on');
-*/
+ 
 define('ADMIN_PANEL', true);
 define('INSTALLER_VERSION', '1.2.##REVISION##');
 

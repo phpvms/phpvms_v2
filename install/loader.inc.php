33cd1af4de01dde054d7081a951a08b3f1a967a5
nshahzad <nabeel@nsslive.net>
2009-11-24 15:58:55 +0000

    Install fixes
    git-svn-id: file:///var/svn/phpvms/trunk@806 e188d3d8-5a48-0410-b2de-ed2772025908

--- install/loader.inc.php
+++ install/loader.inc.php
@@ -25,12 +25,14 @@ define('SITE_ROOT', str_replace('install', '', dirname(__FILE__)));
 define('CORE_PATH', SITE_ROOT . 'core/');
 define('CLASS_PATH', CORE_PATH . 'classes/');
 
-
 if(!file_exists(CORE_PATH.'/local.config.php') || filesize(CORE_PATH.'/local.config.php') == 0)
 {
 	define('DS', DIRECTORY_SEPARATOR);
+	
 	/* Include just some basic files to get the install going */
 	include CLASS_PATH . '/ezDB.class.php';
+	include CLASS_PATH . '/Config.class.php';
+	include CLASS_PATH . '/Debug.class.php';
 	include CLASS_PATH . '/Template.class.php';
 	include CLASS_PATH . '/TemplateSet.class.php';
 	include CLASS_PATH . '/Vars.class.php';

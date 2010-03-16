a9ee3f670318b339e6d5a4814d708530fb525b35
nshahzad <nabeel@nsslive.net>
2010-03-16 16:32:52 +0000

    fixed #215, fixed #204
    git-svn-id: file:///var/svn/phpvms/trunk@901 e188d3d8-5a48-0410-b2de-ed2772025908

---
+++ admin/maintenance.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * phpVMS - Virtual Airline Administration Software
+ * Copyright (c) 2008 Nabeel Shahzad
+ * For more information, visit www.phpvms.net
+ *	Forums: http://www.phpvms.net/forum
+ *	Documentation: http://www.phpvms.net/docs
+ *
+ * phpVMS is licenced under the following license:
+ *   Creative Commons Attribution Non-commercial Share Alike (by-nc-sa)
+ *   View license.txt in the root, or visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+ *
+ * @author Nabeel Shahzad
+ * @copyright Copyright (c) 2008, Nabeel Shahzad
+ * @link http://www.phpvms.net
+ * @license http://creativecommons.org/licenses/by-nc-sa/3.0/
+ */
+
+
+/*	This is the maintenance cron file, which can run nightly. 
+	You should either point to this file directly in your web-host's control panel
+	Or add an entry into the crontab file. I recommend running this maybe 2-3am, 
+ */
+
+include dirname(dirname(__FILE__)).'/core/codon.config.php';
+
+/* Find any retired pilots and set them to retired */
+PilotData::findRetiredPilots();
+
+/* Update any expenses */
+FinanceData::updateAllExpenses();
+
+/* Send any PIREPs to vaCentral which haven't been exported */

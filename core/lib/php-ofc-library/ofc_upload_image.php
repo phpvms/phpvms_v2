767c7ab1253f1eec21461f8e12cfa00467c64129
Nabeel <nshahzad@gmail.com>
2013-09-30 13:15:13 -0400

    Patch for php-ofc-library exploit

--- core/lib/php-ofc-library/ofc_upload_image.php
+++ core/lib/php-ofc-library/ofc_upload_image.php
@@ -6,7 +6,7 @@
 //
 
 /*
- 
+
 print_r( $_GET );
 print_r( $_POST );
 print_r( $_FILES );
@@ -16,14 +16,17 @@ print_r( $GLOBALS["HTTP_RAW_POST_DATA"] );
 
 */
 
+exit(); // NS
 
 // default path for the image to be stored //
 $default_path = '../tmp-upload-images/';
 
 if (!file_exists($default_path)) mkdir($default_path, 0777, true);
 
+// NS
+$name = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_URL);
 // full path to the saved image including filename //
-$destination = $default_path . basename( $_GET[ 'name' ] ); 
+$destination = $default_path . basename( $_GET[ 'name' ] );
 
 echo 'Saving your image to: '. $destination;
 // print_r( $_POST );
@@ -36,9 +39,10 @@ echo 'Saving your image to: '. $destination;
 // the raw bits into $HTTP_RAW_POST_DATA
 //
 
-$jfh = fopen($destination, 'w') or die("can't open file");
+// NS - commented out
+/*$jfh = fopen($destination, 'w') or die("can't open file");
 fwrite($jfh, $HTTP_RAW_POST_DATA);
-fclose($jfh);
+fclose($jfh);*/
 
 //
 // LOOK:
@@ -57,7 +61,7 @@ $default_path = 'tmp-upload-images/';
 if (!file_exists($default_path)) mkdir($default_path, 0777, true);
 
 // full path to the saved image including filename //
-$destination = $default_path . basename( $_FILES[ 'Filedata' ][ 'name' ] ); 
+$destination = $default_path . basename( $_FILES[ 'Filedata' ][ 'name' ] );
 
 // move the image into the specified directory //
 if (move_uploaded_file($_FILES[ 'Filedata' ][ 'tmp_name' ], $destination)) {

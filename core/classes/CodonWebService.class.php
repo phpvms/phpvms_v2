97b626eaf0e019c6a83a56e3ef9156bb0cfeedd2
Nabeel Shahzad <nshahzad@gmail.com>
2017-02-21 17:03:47 -0600

    cleaned up CWS; PSR2 formatting

--- core/classes/CodonWebService.class.php
+++ core/classes/CodonWebService.class.php
@@ -1,7 +1,8 @@
 <?php
+
 /**
  * Codon PHP Framework
- *	www.nsslive.net/codon
+ *    www.nsslive.net/codon
  * Software License Agreement (BSD License)
  *
  * Copyright (c) 2008 Nabeel Shahzad, nsslive.net
@@ -36,254 +37,208 @@
  * @license BSD License
  * @package codon_core
  */
-
 class CodonWebService
 {
-	public $errors = array();
 
-	protected $type = 'curl';
-	protected $curl = null;
-	protected $_separator = '&';
-	protected $curl_exists = true;
+    public $options = [];
+    public $errors = [];
 
-	protected $options = [
-		/*CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; pl; rv:1.9) Gecko/2008052906 Firefox/3.0',*/
-		CURLOPT_FOLLOWLOCATION => true,
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_AUTOREFERER => true,
-		CURLOPT_CONNECTTIMEOUT => 30,
-		CURLOPT_HEADER => false,
-		CURLOPT_FOLLOWLOCATION => true
-	];
+    protected $type = 'curl';
+    protected $curl = null;
+    protected $_separator = '&';
+    protected $curl_exists = true;
 
-	public function __construct()
-	{
-		# Make sure cURL is installed
-
-		if(!function_exists('curl_init'))
-		{
-			$this->curl_exists = false;
-			$this->error('cURL not installed');
-		}
-
-		if($this->curl_exists == true)
-		{
-			if(!$this->curl = curl_init())
-			{
-				$this->error();
-				$this->setType('fopen');
-				$this->curl = null;
-			}
-
-			$this->setType('curl');
-			@curl_setopt_array($this->curl, $this->options);
-		}
-		else
-		{
-			$this->curl = null;
-			$this->setType('fopen');
-		}
-	}
-
-	public function __destruct()
-	{
-		if($this->curl)
-			curl_close($this->curl);
-	}
-
-	/**
-	 * Internal error handler
-	 *
-	 * @param string $txt Error text
-	 */
-	protected function error($txt='')
-	{
-		if($txt != '') {
-			$last = $txt;
-		} else {
-			$last = curl_error($this->curl) .' ('.curl_errno($this->curl).')';
+    public function __construct()
+    {
+        if (!function_exists('curl_init')) {
+            $this->curl_exists = false;
+            $this->error('cURL not installed');
         }
 
-		$this->errors[] = $last;
-	}
+        if (!$this->curl = curl_init()) {
+            $this->error();
+            $this->curl = null;
+        }
 
-	/**
-	 * Set the transfer type (curl or fopen). cURL is better
-	 * POST cannot be done by fopen, it will be ignored
-	 *
-	 * @param string $type curl or fopen
-	 * @return unknown
-	 */
-	public function setType($type = 'curl')
-	{
-		if($type != 'curl' && $type !='fopen')
-		{
-			$this->error('Invalid connection type');
-			return false;
-		}
+        $this->setType('curl');
+        $this->setOptions([
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_AUTOREFERER => true,
+            CURLOPT_CONNECTTIMEOUT => 30,
+            CURLOPT_HEADER => false,
+            CURLOPT_FOLLOWLOCATION => true,
+        ]);
+    }
 
-		$this->type = $type;
-	}
+    public function __destruct()
+    {
+        if ($this->curl) {
+            curl_close($this->curl);
+        }
+    }
 
-	/**
-	 * Set the curl options, that are different from the default
-	 *
-	 * @param unknown_type $opt
-	 * @return unknown
-	 */
-	public function setOptions($opt)
-	{
-		if(!$this->curl)
-		{
-			$this->error('No valid cURL session');
-			return false;
-		}
+    /**
+     * Internal error handler
+     * @param string $txt Error text
+     */
+    protected function error($txt = '')
+    {
+        if ($txt != '') {
+            $last = $txt;
+        } else {
+            $last = curl_error($this->curl) . ' (' . curl_errno($this->curl) . ')';
+        }
 
-		curl_setopt_array($this->curl, $opt);
-	}
+        $this->errors[] = $last;
+    }
 
-	/**
-	 * Grab a URL, but use SSL. Returns the reponse
-	 *
-	 * @param url $url
-	 * @param array $params Associative array of key=value
-	 * @param string $type post or get (get by default)
-	 */
-	public function getSSL($url, $params, $type='get')
-	{
-		// set SSL options and then go
-		curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, 0);
+    /**
+     * Set the transfer type (curl or fopen). cURL is better
+     * POST cannot be done by fopen, it will be ignored
+     * @param string $type curl or fopen
+     * @return null
+     */
+    public function setType($type = 'curl')
+    {
+        if ($type != 'curl' && $type != 'fopen') {
+            $this->error('Invalid connection type');
 
-		$this->get($url, $params, $type);
-	}
+            return false;
+        }
 
-	/**
-	 * Grab a URL, return the reponse
-	 *
-	 * @param url $url
-	 * @param array $params Associative array of key=value
-	 */
-	public function get($url, $params='')
-	{
-		# Builds the parameters list
-		if(is_array($params))
-		{
-			$q_string = '';
-			foreach($params as $name=>$value)
-			{
-				$q_string .= $name.'='.urlencode($value).$this->_separator;
-			}
+        $this->type = $type;
+    }
 
-			$q_string = substr($q_string, 0, strlen($q_string)-1);
-			$url = $url.'?'.$q_string;
-		}
+    /**
+     * Set the curl options, that are different from the default
+     * @param array $opt
+     * @return null
+     */
+    public function setOptions($opt)
+    {
+        if (!$this->curl) {
+            $this->error('No valid cURL session');
 
-		if($this->type == 'fopen')
-		{
-			if(strtolower(ini_get('allow_url_fopen')) == 'on')
-			{
-				return file_get_contents($url);
-			}
-		}
+            return false;
+        }
 
-		if(!$this->curl || $this->curl == null)
-		{
-			$this->error('cURL not installed or initialized!');
-			return false;
-		}
+        curl_setopt_array($this->curl, $opt);
+    }
 
-		curl_setopt ($this->curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt ($this->curl, CURLOPT_URL, $url);
-		if(($ret = curl_exec($this->curl)) === false)
-		{
-			$this->error();
-			return false;
-		}
+    /**
+     * Grab a URL, but use SSL. Returns the reponse
+     *
+     * @param string $url
+     * @param array $params Associative array of key=value
+     * @param string $type post or get (get by default)
+     */
+    public function getSSL($url, $params, $type = 'get')
+    {
+        // set SSL options and then go
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, 0);
+        $this->get($url, $params, $type);
+    }
 
-		return $ret;
-	}
+    /**
+     * Grab a URL, return the reponse
+     * @param string $url
+     * @param array $params Associative array of key=value
+     * @return string
+     */
+    public function get($url, $params = [])
+    {
+        # Builds the parameters list
+        if (is_array($params)) {
+            $q_string = '';
+            foreach ($params as $name => $value) {
+                $q_string .= $name . '=' . urlencode($value) . $this->_separator;
+            }
 
-	/**
-	 * Grab a URL, return the reponse, POST params
-	 *
-	 * @param url $url
-	 * @param array $params Associative array of key=value
-	 */
-	public function post($url, $params='')
-	{
-		if(!$this->curl)
-		{
-			$this->error('cURL not initialized');
-			return false;
-		}
+            $q_string = substr($q_string, 0, strlen($q_string) - 1);
+            $url = $url . '?' . $q_string;
+        }
 
-		//encode our url data properly
-		if(is_array($params))
-		{
-			foreach($params as $key=>$value)
-			{
+        if (!$this->curl || $this->curl == null) {
+            $this->error('cURL not installed or initialized!');
 
-				$cleaned_params[$key] = urlencode($value);
-			}
-		}
-		else
-		{
-			$cleaned_params = urlencode($params);
-		}
+            return false;
+        }
 
-		curl_setopt($this->curl, CURLOPT_POST, 1);
-		curl_setopt($this->curl, CURLOPT_POSTFIELDS, $cleaned_params);
-		curl_setopt ($this->curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt ($this->curl, CURLOPT_URL, $url);
-		if(($ret = curl_exec($this->curl)) === false)
-		{
-			$this->error();
-			return false;
-		}
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        if (($ret = curl_exec($this->curl)) === false) {
+            $this->error();
 
-		return $ret;
-	}
+            return false;
+        }
 
-	/**
-	 * Download a file from $url to the $tofile
-	 *
-	 * @param url $url URL of file to download
-	 * @param path $tofile Path of file to download to
-	 * @return unknown
-	 */
-	public function download($url, $tofile)
-	{
+        return $ret;
+    }
 
-		if($this->type == 'fopen')
-		{
-			if(strtolower(ini_get('allow_url_fopen')) == 'on')
-			{
-				if(file_put_contents($tofile, file_get_contents($url)) == false)
-				{
-					$this->error('Error getting file');
-					return false;
-				}
-			}
-		}
+    /**
+     * Grab a URL, return the reponse, POST params
+     * @param string $url
+     * @param array $params Associative array of key=value
+     * @return bool
+     */
+    public function post($url, $params = [])
+    {
+        if (!$this->curl) {
+            $this->error('cURL not initialized');
 
-		if(!$this->curl || $this->curl == null)
-			return false;
+            return false;
+        }
 
-		if(($fp = fopen($tofile, 'wb')) == false)
-		{
-			$this->error('Error opening '.$tofile);
-			return false;
-		}
+        //encode our url data properly
+        if (is_array($params)) {
+            foreach ($params as $key => $value) {
+                $cleaned_params[$key] = urlencode($value);
+            }
+        } else {
+            $cleaned_params = urlencode($params);
+        }
 
-		curl_setopt($this->curl, CURLOPT_BINARYTRANSFER, true);
-		curl_setopt($this->curl, CURLOPT_FILE, $fp);
-		curl_setopt ($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_POST, 1);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $cleaned_params);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        if (($ret = curl_exec($this->curl)) === false) {
+            $this->error();
 
-		if(($ret = curl_exec($this->curl)) === false)
-		{
-			$this->error();
-			unlink($tofile);
-			return false;
-		}
-	}
+            return false;
+        }
+
+        return $ret;
+    }
+
+    /**
+     * Download a file from $url to the $tofile
+     * @param string $url URL of file to download
+     * @param string $tofile Path of file to download to
+     * @return bool
+     */
+    public function download($url, $tofile)
+    {
+        if (!$this->curl || $this->curl == null) {
+            return false;
+        }
+
+        if (($fp = fopen($tofile, 'wb')) == false) {
+            $this->error('Error opening ' . $tofile);
+
+            return false;
+        }
+
+        curl_setopt($this->curl, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($this->curl, CURLOPT_FILE, $fp);
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+
+        if (($ret = curl_exec($this->curl)) === false) {
+            $this->error();
+            unlink($tofile);
+
+            return false;
+        }
+    }
 }
